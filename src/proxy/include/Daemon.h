ae59ba4876bd7c1783006946356a6f87d51e65d8
zgjsxx <11916052@qq.com>
2022-11-17 02:47:30 -0500

    remove old code

--- src/proxy/include/Daemon.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Daemon
-{
-public:
-   static void start();
-   static void daemonize();
-   static void initStateThread();
-};
--- src/proxy/include/SocketHelper.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class SocketHelper
-{
-public:
-    static int createProxySocket(const int port);
-    static void ProxyListen(int serverSocket);
-};
