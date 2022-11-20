cc619ad480321aad84478d778e06c1d85a4128ad
zgjsxx <119160524@qq.com>
2022-11-20 11:52:45 +0800

    update bench code

--- bench/main.go
+++ bench/main.go
@@ -1,50 +1,77 @@
 package main
 
 import (
+  "bufio"
+  "io"
+  "os"    
   "fmt"
   "crypto/tls"
   "net/http"
   "sync"
   _ "io/ioutil"
   "bench/Util"
+  "time"
+  "math/rand"
 )
 
 var wg sync.WaitGroup
-
+var num int = 100
 func testUrl(){
     var PTransport = & http.Transport {
         Proxy: http.ProxyFromEnvironment,
         TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-      }
-      client := http.Client {
+        MaxIdleConns:          5,
+    }
+    client := http.Client {
         Transport: PTransport,
-      }
+    }
     
-      for _, val := range Util.Testurl {
+    for i := 0; i < num; i++ {
+        size := len(Util.Testurl)
+        x1 := rand.NewSource(time.Now().UnixNano())
+        y1 := rand.New(x1)
+        index := y1.Intn(size)
+        
+        val := Util.Testurl[index]
+        fmt.Printf("index = %d, test url = %s\n", index, val) 
+
         req, err := http.NewRequest("GET", val, nil)
-        // req.Header.Add("If-None-Match", `some value`)
         resp, err := client.Do(req)
         if err != nil {
-        //   panic(err)
-            fmt.Printf("err \n")
+            fmt.Printf("%s err \n", val)
             continue
+            time.Sleep(1 * time.Second)
         }
-        fmt.Printf("GET Response = %s \n", resp.Status)
-        // defer resp.Body.Close()
-      
-        // bodyBytes, err := ioutil.ReadAll(resp.Body)
-        // if err != nil {
-        //   panic(err)
-        // }
-      
-        // bodyString := string(bodyBytes)
-        // fmt.Printf("GET Response = %s \n", string(bodyString))
-      }
-      wg.Done()
+        fmt.Printf("%s GET Response = %s \n", val, resp.Status)
+        time.Sleep(1 * time.Second)
+        defer resp.Body.Close()
+    }
+
+    wg.Done()
+}
+
+func prepare_test_url() {
+    fi, err := os.Open("url.txt")
+    if err != nil {
+        fmt.Printf("Error: %s\n", err)
+        return
+    }
+    defer fi.Close()
+
+    br := bufio.NewReader(fi)
+    for {
+        a, _, c := br.ReadLine()
+        if c == io.EOF {
+            break
+        }
+        fmt.Println(string(a))
+        Util.Testurl = append(Util.Testurl, string(a))
+    }
 }
 
 func main() {
-    for i := 0; i < 3000; i++ {
+    prepare_test_url()
+    for i := 0; i < 10; i++ {
         wg.Add(1)
         go testUrl()
     }
